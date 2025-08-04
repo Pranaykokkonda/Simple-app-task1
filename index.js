a9bfe408e491ed4896b4b28bdf60fe7b1e096f05
Pranaykokkonda <pranay.kokkonda22@gmail.com>
2025-08-04 14:12:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Node.js demo app!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
-
-module.exports = app; // Export for testing
+module.exports = app;
