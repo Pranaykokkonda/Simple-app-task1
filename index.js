22dbe7e102754838913bf5fa0c466d24a2a820cd
Pranaykokkonda <pranay.kokkonda22@gmail.com>
2025-08-04 19:41:58 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js demo app!');
+  res.send('You Have Successfully Deployed The Node.js Application');
 });
 
 module.exports = app;
