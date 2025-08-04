18a7ae241a2002fd1558c90d9f54f6a3159ae4b4
Pranaykokkonda <pranay.kokkonda22@gmail.com>
2025-08-04 19:21:20 +0530

    Updated app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('responds with Hello from Node.js demo app!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello from Node.js demo app!');
+    expect(res.text).toBe('You Have Successfully Deployed The Node.js Application');
   });
 });
